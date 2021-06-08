79a72f38846936731f98a134172520a8b0903c0b
Carl Tape <carltape@gmail.com>
2021-06-08 09:45:31 -0800

    fix paths for outside-repo functions

--- matlab/load_mt.m
+++ matlab/load_mt.m
@@ -1,19 +1,21 @@
 %
 % load_mt.m
 %
-% Set paths to additional directories in compearth
-% (These are needed for utilities such as rotation scripts.)
+% Set paths to additional utilities (such as rotation scripts) found in the
+% repository surfacevel2strain.
+% Get the repo by cloning it at the same location as mtbeach:
+% git clone --depth=1 https://github.com/carltape/surfacevel2strain
 %
 % load_mt.m can be called from a matlab startup.m script,
 % so that you never need to execute it.
 
-% path to the compearth head directory 
-% it is safest to specify an absolute path to your compearth directory
-%compearth = strcat(getenv('HOME'),'/REPOSITORIES/compearth/');
-% this relative path will only work if you run Matlab from compearth/momenttensor/matlab
-compearth = '../../';
+% path to the directory containing mtbeach and surfacevel2strain
+% it is safest to specify an absolute path such as
+%repopath = strcat(getenv('HOME'),'/REPOSITORIES/');
+% this relative path will only work if you run Matlab from mtbeach/matlab/
+repopath = '../../';
 
-addpath(strcat(compearth,'surfacevel2strain/matlab/util_basic'));
-addpath(strcat(compearth,'surfacevel2strain/matlab/util_euler'));
-addpath(strcat(compearth,'momenttensor/matlab'));
-addpath(strcat(compearth,'momenttensor/matlab/Vhat'));
+addpath(strcat(repopath,'surfacevel2strain/matlab/util_basic'));
+addpath(strcat(repopath,'surfacevel2strain/matlab/util_euler'));
+addpath(strcat(repopath,'mtbeach/matlab'));
+addpath(strcat(repopath,'mtbeach/matlab/Vhat'));
